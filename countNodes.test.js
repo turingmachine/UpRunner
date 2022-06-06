e1edecce5b34e0d115f74406f3d631f077ae061f
Marlen Buri <marlen@yokto.net>
2022-06-06 14:20:43 +0200

    fix test description

--- countNodes.test.js
+++ countNodes.test.js
@@ -5,7 +5,7 @@ test("calculate 365 days starting with 1000 nodes", () => {
   expect(nodes).toBe(1829);
 });
 
-test("calculate 365 days starting with 1000 nodes", () => {
+test("calculate 40 days starting with 1537 nodes", () => {
   const nodes = countNodes(15100, 25, 40, 1537);
   expect(nodes).toBe(1642);
 });
