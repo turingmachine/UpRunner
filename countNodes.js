8d53bbb7e1887b7746a8605231394f69a3628348
Simon Josi <me@yokto.net>
2022-06-12 21:54:51 +0200

    ad jupyter notebook

--- countNodes.js
+++ countNodes.js
@@ -12,3 +12,13 @@ function calculateNodes(stake, pokt, targetDays,startNodes) {
   return currentNode
 }
 module.exports=calculateNodes;
+
+const year1 = calculateNodes(15100, 25, 90, 1000)
+const year2 = calculateNodes(15100, 25, 365, year1)
+const year3 = calculateNodes(15100, 25, 365, year2)
+const year4 = calculateNodes(15100, 25, 365, year3)
+const year5 = calculateNodes(15100, 25, 365, year4)
+const year6 = calculateNodes(15100, 25, 365, year5)
+
+
+console.log(year1, year2, year3, year4, year5, year6)
